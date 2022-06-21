17d49fd4bc86a93ff24713924bec1de21764b5d3
Aphaki <kiapha1108@gmail.com>
2022-06-21 16:21:50 +0900

    Initial Commit

---
+++ CoinImgWithFileManager/CoinImgWithFileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinImgWithFileManagerApp.swift
+//  CoinImgWithFileManager
+//
+//  Created by Sy Lee on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CoinImgWithFileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
